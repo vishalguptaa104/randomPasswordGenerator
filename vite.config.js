7f632bba1ee7d79889201f56d5d07bc73b427006
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-04-07 18:52:46 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/",
+  base : "/randomPasswordGenerator/",
   plugins: [react()],
   test: {
     globals: true,
