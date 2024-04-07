8579a3083df9313599d63ebef0924f5bc3184f0b
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-04-07 17:38:24 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 
-
 function App() {
-
+  
   const [length, setLength] = useState(8)
   const [numAllowed, setNumAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
